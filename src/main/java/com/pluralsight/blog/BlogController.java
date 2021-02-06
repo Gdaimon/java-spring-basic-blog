9d358c61ab7c261e0a996fa1740752d3de465b9f
Carlos Andres <darkklitos@gmail.com>
2021-02-06 08:03:20 -0500

    Add method and data view home

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,19 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class BlogController {
-
+	
+	@RequestMapping ( "/" )
+	public String listPost ( ) {
+		Map < String, String > map = new HashMap <> ( );
+		map.put ( "title", "Blog Post 1" );
+		return "home";
+	}
+	
 }
