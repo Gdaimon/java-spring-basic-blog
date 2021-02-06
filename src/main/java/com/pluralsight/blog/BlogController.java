ee42b50ba39289ff0326db83985a6ef5e7ec81ab
Carlos Andres <darkklitos@gmail.com>
2021-02-06 08:14:27 -0500

    adjust param ModelMap

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,18 +1,15 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class BlogController {
 	
 	@RequestMapping ( "/" )
-	public String listPosts ( ) {
-		Map < String, String > map = new HashMap <> ( );
-		map.put ( "title", "Blog Post 1" );
+	public String listPosts ( ModelMap modelMap ) {
+		modelMap.put ( "title", "Blog Post 1" );
 		return "home";
 	}
 	
